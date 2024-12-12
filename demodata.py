e58a1d486e466d404afe18df1c645a4213bbbf8e
Aayesha-Aayesha <153555591+Aayesha-Aayesha@users.noreply.github.com>
2024-12-12 17:38:10 +0000

    new file and content added

--- demodata.py
+++ demodata.py
@@ -1,5 +1,13 @@
 
 import pandas as pd
+from flask import Flask, render_template
 df = pd.read_csv("data.csv")
 print(df)
 
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
